a3f899d1c39601f669e108c19c1ea7f01ff5e389
Rumail Memon <36772370+RumailM@users.noreply.github.com>
2022-12-09 17:47:49 -0500

    functionality implemented with helper functions
    to do: 
    tui

--- main.go
+++ main.go
@@ -8,20 +8,122 @@ import (
 	"strings"
 )
 
+func create_launch(path string, template_controller_name string, new_controller_name string) (err_ error) {
+	//copy file first
+	fmt.Println(path + template_controller_name + ".launch")
+	source, err := os.Open(path + template_controller_name + ".launch")
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+	fmt.Println("Created and Modified all Files\n")
+
+	destination, err := os.Create(path + new_controller_name + ".launch")
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	scanner := bufio.NewScanner(source) //create scanner for the new file
+
+	for scanner.Scan() {
+
+		if strings.Contains(scanner.Text(), template_controller_name) {
+			destination.WriteString(strings.Replace(scanner.Text(), template_controller_name, new_controller_name, -1) + "\n")
+		} else if !(strings.Contains(scanner.Text(), "</launch>")) {
+			destination.WriteString(scanner.Text() + "\n")
+		} else {
+			destination.WriteString(scanner.Text())
+		}
+
+	}
+
+	return err
+}
+
+func create_source(path string, template_controller_name string, new_controller_name string, template_code_name string, new_code_name string) (err_ error) {
+	//copy file first
+	source, err := os.Open(path + template_controller_name + ".cpp")
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(path + new_controller_name + ".cpp")
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	scanner := bufio.NewScanner(source) //create scanner for the new file
+
+	for scanner.Scan() {
+
+		if strings.Contains(scanner.Text(), template_code_name) {
+			destination.WriteString(strings.Replace(scanner.Text(), template_code_name, new_code_name, -1) + "\n")
+		} else if !(strings.Contains(scanner.Text(), "PLUGINLIB_EXPORT_CLASS")) {
+			destination.WriteString(scanner.Text() + "\n")
+		} else {
+			destination.WriteString(scanner.Text())
+		}
+
+	}
+
+	return err
+}
+
+func create_header(path string, template_controller_name string, new_controller_name string, template_code_name string, new_code_name string) (err_ error) {
+	//copy file first
+	source, err := os.Open(path + template_controller_name + ".h")
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(path + new_controller_name + ".h")
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	scanner := bufio.NewScanner(source) //create scanner for the new file
+
+	for scanner.Scan() {
+
+		if strings.Contains(scanner.Text(), template_code_name) {
+			destination.WriteString(strings.Replace(scanner.Text(), template_code_name, new_code_name, -1) + "\n")
+		} else if !(strings.Contains(scanner.Text(), "// namespace fr3_ros")) {
+			destination.WriteString(scanner.Text() + "\n")
+		} else {
+			destination.WriteString(scanner.Text())
+		}
+
+	}
+
+	return err
+}
+
 func add_controller_yaml(path string, template_controller_name string, new_controller_name string) (err_ error) {
 	//open main yaml file, create a temp file and a new destination file
 
-	f, err1 := os.Open(path + "fr3_ros.yaml")
+	e := os.Rename(path+"fr3_ros.yaml", path+"fr3_ros.yaml"+".bak")
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	f, err1 := os.Open(path + "fr3_ros.yaml.bak")
 	if err1 != nil {
 		return err1
 	}
 	defer f.Close()
+
 	f2, err2 := os.Create(path + "fr3_ros_temp.yaml")
 	if err2 != nil {
 		return err2
 	}
 	defer f2.Close()
-	f3, err3 := os.Create(path + "fr3_ros_new.yaml")
+
+	f3, err3 := os.Create(path + "fr3_ros.yaml")
 	if err3 != nil {
 		return err3
 	}
@@ -29,21 +131,27 @@ func add_controller_yaml(path string, template_controller_name string, new_contr
 
 	scanner := bufio.NewScanner(f) //create scanner for the main file
 	reading_template_flag := 0
+	second_line_flag := 0
 
 	for scanner.Scan() {
 		if strings.Contains(scanner.Text(), template_controller_name) { // find where the template controller  is
 
-			f2.WriteString(new_controller_name + "\n")
+			f2.WriteString(new_controller_name + ":\n")
+			f2.WriteString("    type: fr3_ros/" + new_controller_name + "\n")
 			reading_template_flag = 1
+			second_line_flag = 1
 			continue
 		}
 		if reading_template_flag == 1 { //once we find it copy the template controller with new name into temp file
+			if second_line_flag == 1 {
+				second_line_flag = 0
+				continue
+			}
 			f2.WriteString(scanner.Text() + "\n")
 		}
 
 		if len(scanner.Text()) == 0 && reading_template_flag == 1 {
 			reading_template_flag = 0
-			fmt.Println("dolol\n")
 			break
 		}
 
@@ -53,7 +161,7 @@ func add_controller_yaml(path string, template_controller_name string, new_contr
 	f.Close()
 	f2.Close()
 
-	f_, err1 := os.Open(path + "fr3_ros.yaml")
+	f_, err1 := os.Open(path + "fr3_ros.yaml.bak")
 	if err1 != nil {
 		return err1
 	}
@@ -86,7 +194,7 @@ func add_controller_yaml(path string, template_controller_name string, new_contr
 	f3.Close()
 
 	//delete temp file
-	e := os.Remove(path + "fr3_ros_temp.yaml")
+	e = os.Remove(path + "fr3_ros_temp.yaml")
 	if e != nil {
 		log.Fatal(e)
 	}
@@ -94,26 +202,33 @@ func add_controller_yaml(path string, template_controller_name string, new_contr
 
 }
 
-func add_plugin(path string, controller_name string, controller_description string) (err_ error) {
-	f, err1 := os.Open(path + "fr3_ros_plugin.xml")
+func add_plugin(path string, new_controller_name string, controller_description string) (err_ error) {
+	e := os.Rename(path+"fr3_ros_plugin.xml", path+"fr3_ros_plugin.xml"+".bak")
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	f, err1 := os.Open(path + "fr3_ros_plugin.xml.bak")
 	if err1 != nil {
 		return err1
 	}
 	defer f.Close()
-	f2, err2 := os.Create(path + "fr3_ros_plugin_new.xml")
+	f2, err2 := os.Create(path + "fr3_ros_plugin.xml")
 	if err2 != nil {
 		return err2
 	}
 	defer f2.Close()
 
-	xml_template := "  <class name=\"fr3_ros/" + controller_name + "\" type=\"fr3_ros::" + controller_name + "\" base_class_type=\"controller_interface::ControllerBase\">\n    <description>\n" + "      " + controller_description + "\n    </description>\n  </class>"
+	xml_template := "  <class name=\"fr3_ros/" + new_controller_name + "\" type=\"fr3_ros::" + new_controller_name + "\" base_class_type=\"controller_interface::ControllerBase\">\n    <description>\n" + "      " + controller_description + "\n    </description>\n  </class>"
 
 	scanner := bufio.NewScanner(f)
 	scanner.Scan()
 	f2.WriteString(scanner.Text() + "\n")
-	f2.WriteString(xml_template + "\n")
 
 	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), "</library>") {
+			f2.WriteString(xml_template + "\n")
+		}
 		f2.WriteString(scanner.Text() + "\n")
 	}
 
@@ -123,44 +238,67 @@ func add_plugin(path string, controller_name string, controller_description stri
 
 }
 func add_lib_CMAKE(path string, controller_name string) (err_ error) {
-	f, err1 := os.Open(path + "CMakeLists.txt")
-	if err1 != nil {
-		return err1
+
+	e := os.Rename(path+"CMakeLists.txt", path+"CMakeLists.txt"+".bak")
+	if e != nil {
+		log.Fatal(e)
 	}
-	defer f.Close()
-	f2, err2 := os.Create(path + "CMakeLists_new.txt")
+
+	f2, err2 := os.Create(path + "CMakeLists.txt")
 	if err2 != nil {
 		return err2
 	}
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
+
+	source, err := os.Open(path + "CMakeLists.txt.bak")
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(source)
 
 	for scanner.Scan() {
-		f2.WriteString("scanner.Text()+\n")
+		f2.WriteString(scanner.Text() + "\n")
 		if strings.Contains(scanner.Text(), "add_library") {
 			// fmt.Println(scanner.Text())
 			f2.WriteString("  src/" + controller_name + "\n")
 		}
 	}
 
-	f.Close()
+	source.Close()
 	f2.Close()
+
 	return
 
 }
 func main() {
 
-	// path_var := "/home/rum/fr3_ros/fr3_ros/"
-	// lib_var := "sweeetlibraryname"
-	// status := add_lib_CMAKE(path_var, lib_var)
-	// fmt.Println(status)
-	// path_var1 := "/home/rum/fr3_ros/fr3_ros/"
-	// lib_var2 := "sweeetlibraryname"
-	// status3 := add_plugin(path_var1, lib_var2, "new_desc")
-	path_var5 := "/home/rum/fr3_ros/fr3_ros/config/"
-	template_controller_name := "qp_controller:"
-	new_controller_name := "joint_ppppp_controller"
-	status4 := add_controller_yaml(path_var5, template_controller_name, new_controller_name)
-	fmt.Println(status4)
+	cwd_, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(cwd_)
 
+	path_CMake := cwd_ + "/fr3_ros/"
+	path_plugin := path_CMake
+	path_yaml := cwd_ + "/fr3_ros/config/"
+	path_header := cwd_ + "/fr3_ros/include/fr3_ros/"
+	path_source := cwd_ + "/fr3_ros/src/"
+	path_launch := cwd_ + "/fr3_ros/launch/"
+
+	template_controller_name := "joint_pd_controller"
+	template_controller_code_name := "JointPDController"
+
+	new_controller_name := "bololo_controller"
+	new_controller_code_name := "JJJJOPController"
+
+	new_controller_description := "This is an overpowered controller"
+
+	_ = add_lib_CMAKE(path_CMake, new_controller_name)
+	_ = add_plugin(path_plugin, new_controller_name, new_controller_description)
+	_ = add_controller_yaml(path_yaml, template_controller_name, new_controller_name)
+	_ = create_header(path_header, template_controller_name, new_controller_name, template_controller_code_name, new_controller_code_name)
+	_ = create_source(path_source, template_controller_name, new_controller_name, template_controller_code_name, new_controller_code_name)
+	_ = create_launch(path_launch, template_controller_name, new_controller_name)
+
+	fmt.Println("Created and Modified all Files\n")
 }
