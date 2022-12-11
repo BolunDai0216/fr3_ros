4e0aab8ec146a58099cfb9226b0b7d1028efdf12
Rumail Memon <36772370+RumailM@users.noreply.github.com>
2022-12-10 20:20:56 -0500

    cleaned up controller helper

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
@@ -34,11 +36,12 @@ type model struct {
 	textInput              textinput.Model
 	controller_description textinput.Model
 	custom_cpp_name        textinput.Model
+	custom_class_name      textinput.Model
 	template_file_names    []string
 
 	template_controller textinput.Model
 	err                 error
-	prog_flow           int
+	state               string
 	cpp_names           []string
 	cursor              int
 	cursor2             int
@@ -49,6 +52,7 @@ type model struct {
 	path_header         string
 	path_source         string
 	path_launch         string
+	path_backup         string
 }
 
 func initialModel() model {
@@ -76,31 +80,47 @@ func initialModel() model {
 	ti3.TextStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("87"))
 	ti3.SetValue(ti3.Placeholder)
 
+	ti4 := textinput.New()
+	ti4.Placeholder = "CustomCppControllerName"
+	// ti3.Focus()
+	ti4.CharLimit = 156
+	ti4.Width = 100
+	ti4.TextStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("87"))
+	ti4.SetValue(ti4.Placeholder)
+
 	cwd_, err := os.Getwd()
 	if err != nil {
 		log.Println(err)
 	}
 
+	var temp_arr [2]string
+	temp_arr[0] = ""
+	temp_arr[1] = "Custom"
 	return model{
 		textInput:              ti,
 		controller_description: ti1,
 		custom_cpp_name:        ti3,
+		custom_class_name:      ti4,
 		err:                    nil,
-		prog_flow:              0,
+		state:                  "input_controller_name",
+		template_file_names:    temp_arr[:],
+		cpp_names:              temp_arr[:],
 		curentdir:              cwd_,
+		cursor:                 0,
 		path_CMake:             cwd_ + "/fr3_ros/",
 		path_plugin:            cwd_ + "/fr3_ros/",
 		path_yaml:              cwd_ + "/fr3_ros/config/",
 		path_header:            cwd_ + "/fr3_ros/include/fr3_ros/",
 		path_source:            cwd_ + "/fr3_ros/src/",
 		path_launch:            cwd_ + "/fr3_ros/launch/",
+		path_backup:            cwd_ + "/backup/",
 	}
 }
 
 func getClassName(template_controller_name string, path string) (class_name string, err_ error) {
 	var temp_arr []string
 	var temp string
-	fmt.Println(path + template_controller_name + ".h")
+	// fmt.Println(path + template_controller_name + ".h")
 	source1, err := os.Open(path + template_controller_name + ".h")
 	if err != nil {
 		return "s", err
@@ -111,7 +131,7 @@ func getClassName(template_controller_name string, path string) (class_name stri
 	for scanner.Scan() {
 
 		if strings.Contains(scanner.Text(), "class") {
-			fmt.Println(scanner.Text())
+			// fmt.Println(scanner.Text())
 
 			temp = scanner.Text()
 			break
@@ -120,7 +140,7 @@ func getClassName(template_controller_name string, path string) (class_name stri
 	}
 
 	temp_arr = strings.Fields(temp)
-	fmt.Println(temp_arr)
+	// fmt.Println(temp_arr)
 
 	source1.Close()
 	return temp_arr[1], err
@@ -128,11 +148,26 @@ func getClassName(template_controller_name string, path string) (class_name stri
 }
 
 func populateTemplateChoices(path string) (choices []string) {
+
+	blacklist := [...]string{"cbf_utils", "controller_utils", "pinocchio_utils", "traj_gen", "visualization_utils"}
+	skip_flag := 0
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, files := range files {
+		// fmt.Println()
+		skip_flag = 0
+
+		for j, _ := range blacklist {
+			if strings.Contains(files.Name(), blacklist[j]) {
+				skip_flag = 1
+				break
+			}
+		}
+		if skip_flag == 1 {
+			continue
+		}
 		choices = append(choices, strings.Replace(files.Name(), ".cpp", "", -1))
 	}
 	return choices
@@ -140,16 +175,16 @@ func populateTemplateChoices(path string) (choices []string) {
 
 func populateChoices(user_input_controller_name string) (choices []string) {
 
-	choices = append(choices, user_input_controller_name)
+	// choices = append(choices, user_input_controller_name)
 	choices = append(choices, strings.Replace(cases.Title(language.Und, cases.NoLower).String(strings.Replace(user_input_controller_name, "_", " ", -1)), " ", "", -1))
-	temp_string_arr := strings.Split(user_input_controller_name, "_")
-	temp_string := strings.ToUpper(temp_string_arr[0])
-	if len(temp_string) > 1 {
-		for i := 1; i < len(temp_string_arr); i++ {
-			temp_string += cases.Title(language.Und, cases.NoLower).String(temp_string_arr[i])
-		}
-	}
-	choices = append(choices, temp_string)
+	// temp_string_arr := strings.Split(user_input_controller_name, "_")
+	// temp_string := strings.ToUpper(temp_string_arr[0])
+	// if len(temp_string) > 1 {
+	// 	for i := 1; i < len(temp_string_arr); i++ {
+	// 		temp_string += cases.Title(language.Und, cases.NoLower).String(temp_string_arr[i])
+	// 	}
+	// }
+	// choices = append(choices, temp_string)
 	choices = append(choices, "Custom")
 	return choices
 
@@ -168,69 +203,52 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
 		case tea.KeyEnter:
-			switch m.prog_flow {
-			case 0: //user chose controller name
-				m.prog_flow = 1
-				m.cpp_names = populateChoices(m.textInput.Value())
+			switch m.state {
+			case "input_controller_name":
+				m.state = "input_class_name"
 				m.textInput.Blur()
-				// return m, cmd
-			case 1: //user selected cpp name
-				if m.cursor != len(m.cpp_names)-1 {
-					m.prog_flow = 3
-					m.controller_description.Focus()
-				} else {
-					m.prog_flow = 2
-					m.custom_cpp_name.Focus()
-				}
-			case 2:
-				m.custom_cpp_name.Blur()
+				m.custom_class_name.Focus()
+				m.cpp_names = populateChoices(m.textInput.Value())
+				m.custom_class_name.SetValue(m.cpp_names[0])
+			case "input_class_name":
+				m.state = "input_description"
+				m.cpp_names[0] = m.custom_class_name.Value()
+				m.custom_class_name.Blur()
 				m.controller_description.Focus()
-				m.cpp_names[len(m.cpp_names)-1] = m.custom_cpp_name.Value()
-				m.prog_flow = 3
-			case 3:
+			case "input_description":
+				m.state = "choose_template"
 				m.template_file_names = populateTemplateChoices(m.path_source)
 				m.controller_description.Blur()
-				m.prog_flow = 4
-			case 4:
-				m.prog_flow = 5
+			case "choose_template":
+				m.state = "final_confirmation"
 
-			case 5:
-				template_controller_code_name, _ := getClassName(m.template_file_names[m.cursor2], m.path_header)
-				_ = add_lib_CMAKE(m.path_CMake, m.textInput.Value())
-				_ = add_plugin(m.path_plugin, m.textInput.Value(), m.controller_description.Value())
-				_ = add_controller_yaml(m.path_yaml, m.template_file_names[m.cursor2], m.textInput.Value())
-				_ = create_header(m.path_header, m.template_file_names[m.cursor2], m.textInput.Value(), template_controller_code_name, m.cpp_names[m.cursor])
-				_ = create_source(m.path_source, m.template_file_names[m.cursor2], m.textInput.Value(), template_controller_code_name, m.cpp_names[m.cursor])
-				_ = create_launch(m.path_launch, m.template_file_names[m.cursor2], m.textInput.Value())
-				// fmt.Println(temps)
-				m.prog_flow = 6
-			case 6:
-				return m, tea.Quit
-
-			}
-		default:
-			switch m.prog_flow {
-			case 0:
-				m.textInput, cmd = m.textInput.Update(msg)
-			case 1:
-				switch msg.String() {
-				case "up", "k":
-					if m.cursor > 0 {
-						m.cursor--
-					}
-
-				// The "down" and "j" keys move the cursor down
-				case "down", "j":
-					if m.cursor < len(m.cpp_names)-1 {
-						m.cursor++
+			case "final_confirmation":
+				if _, err := os.Stat("backup"); errors.Is(err, os.ErrNotExist) {
+					err := os.Mkdir("backup", os.ModePerm)
+					if err != nil {
+						log.Println(err)
 					}
 				}
-			case 2:
-				m.custom_cpp_name, cmd = m.custom_cpp_name.Update(msg)
-			case 3:
+				m.state = "finished"
+				template_controller_code_name, _ := getClassName(m.template_file_names[m.cursor], m.path_header)
+				_ = add_lib_CMAKE(m.path_CMake, m.path_backup, m.textInput.Value())
+				_ = add_plugin(m.path_plugin, m.path_backup, m.textInput.Value(), m.controller_description.Value())
+				_ = add_controller_yaml(m.path_yaml, m.path_backup, m.template_file_names[m.cursor], m.textInput.Value())
+				_ = create_header(m.path_header, m.template_file_names[m.cursor], m.textInput.Value(), template_controller_code_name, m.custom_class_name.Value())
+				_ = create_source(m.path_source, m.template_file_names[m.cursor], m.textInput.Value(), template_controller_code_name, m.custom_class_name.Value())
+				_ = create_launch(m.path_launch, m.template_file_names[m.cursor], m.textInput.Value())
+			case "finished":
+				return m, tea.Quit
+			}
+		default:
+			switch m.state {
+			case "input_controller_name":
+				m.textInput, cmd = m.textInput.Update(msg)
+			case "input_class_name":
+				m.custom_class_name, cmd = m.custom_class_name.Update(msg)
+			case "input_description":
 				m.controller_description, cmd = m.controller_description.Update(msg)
-			case 4:
-
+			case "choose_template":
 				switch msg.String() {
 				case "up", "k":
 					if m.cursor2 > 0 {
@@ -242,7 +260,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					if m.cursor2 < len(m.template_file_names)-1 {
 						m.cursor2++
 					}
+
 				}
+				if m.cursor2 < (len(m.template_file_names)+1)/2 {
+					m.cursor = m.cursor2 * 2
+				} else {
+					m.cursor = (m.cursor2-((len(m.template_file_names)-1)/2))*2 - 1
+				}
+			case "final_step":
+
 			}
 
 		}
@@ -252,105 +278,116 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.err = msg
 		return m, nil
 	}
-	// switch m.prog_flow {
-	// case 0:
-
-	// case 2:
-	// 	m.custom_cpp_name, cmd = m.custom_cpp_name.Update(msg)
-
-	// }
 
 	return m, cmd
 }
 
 func (m model) View() string {
-	// fmt.Println(m.textInput.Value())
 	s := ""
-	switch m.prog_flow {
-	case 6:
-		s += "Finished!!!\n"
-	case 5:
-		s += "You chose your controller name as > \"" + m.textInput.Value() + "\"\n"
-		s += "You chose your controller .cpp name as > \"" + m.cpp_names[m.cursor] + ".cpp\"\n"
-		s += "Your controller description is> \n   \""
-		s += m.controller_description.Value() + "\"\n\n"
-		s += "You chose the template as > \"" + m.template_file_names[m.cursor2] + "\"\n\n"
-		s += "If all of this information is correct, press enter to generate starter files for your new controller! \nOtherwise press esc to try again"
-		s += "\n\n(esc to quit)"
-	case 4:
-		s += "You chose your controller name as > \"" + m.textInput.Value() + "\"\n"
-		s += "You chose your controller .cpp name as > \"" + m.cpp_names[m.cursor] + ".cpp\"\n"
-		s += "Your controller description is> \n   \""
-		s += m.controller_description.Value() + "\"\n\n"
+
+	x := [...]string{"You chose your controller name as > \"" + m.textInput.Value() + "\"\n",
+		"You chose your controller .cpp name as > \"" + m.custom_class_name.Value() + ".cpp\"\n",
+		"Your controller description is> \n   \"" + m.controller_description.Value() + "\"\n\n",
+		"You chose the template as > \"" + m.template_file_names[m.cursor] + "\"\n\n"}
+	tail := "\n\n[Enter] to continue, [Esc] to quit)"
+	switch m.state {
+	case "finished":
+		s = "Success! Press [Enter] button to quit\n"
+	case "final_confirmation":
+
+		s = ""
+		for i := 0; i < 4; i++ {
+			s += x[i]
+		}
+		s += "If all of this information is correct, press [Enter] to generate starter files for your new controller! \nOtherwise press [Esc] to try again"
+		s += tail
+	case "choose_template":
+		prev_len := 0
+		temp := ""
+		s = ""
+		for i := 0; i < 4; i++ {
+			s += x[i]
+		}
 		s += "Finally, please choose the template controller you'd like to use as a starting point for your new controller>\n\n"
+
 		for i, choice := range m.template_file_names {
 			cursor2 := " "
-			if m.cursor2 == i {
+			// if m.cursor2 == i {
+			// 	cursor2 = ">"
+			// }
+
+			checked := " "
+
+			// if i < (len(m.template_file_names)+1)/2 {
+			if m.cursor2 == i/2.0 && i%2 == 0 {
 				cursor2 = ">"
+				// m.cursor = i
+			} else {
+				if i%2 == 1 && m.cursor2 == (len(m.template_file_names)+1)/2+(i-1)/2 {
+					cursor2 = ">"
+					// m.cursor = i
+				}
 			}
-			checked := " "
 
-			s += fmt.Sprintf("%s [%s] %s.cpp\n", cursor2, checked, choice)
+			// }
+			if i%2 == 0 {
+
+				temp = fmt.Sprintf("%s [%s] %s.cpp", cursor2, checked, choice)
+				s += temp
+				prev_len = utf8.RuneCountInString(temp)
+			} else {
+
+				// if m.cursor2 == (len(m.template_file_names)+1)/2+i {
+				// 	cursor2 = ">"
+				// }
+				s += strings.Repeat(" ", (50-prev_len)) + fmt.Sprintf("%s [%s] %s.cpp\n", cursor2, checked, choice)
+			}
+
 		}
-		s += "\n\n\n(use up and down arrows to select, enter to enter, esc to quit)\n"
+		s += "\n\n\n(use [Up] and [Down] arrows to select, [Enter] to select, [Esc] to quit)\n"
 
-	case 3:
+	case "input_description":
+		s = ""
+		for i := 0; i < 3; i++ {
+			s += x[i]
+		}
 
 		s += fmt.Sprintf(
-			"%s%s%s%s%s",
-			"You chose your controller name as > \""+m.textInput.Value()+"\"\n",
-			"You chose your controller .cpp name as > \""+m.cpp_names[m.cursor]+".cpp\"\n\n",
+			"\n%s%s",
 			"Almost there! Now please provide a one line description of your new controller\n\n",
-			m.controller_description.View(),
-			"\n\n(esc to quit)")
+			m.controller_description.View())
+		s += tail
 
-	case 2:
-		s += fmt.Sprintf("%s%s%s%s",
-			"You chose your controller name as > \""+m.textInput.Value()+"\"\n\n",
-			"You opted to have a custom name for the cpp class for your controller, please enter it:\n\n",
-			m.custom_cpp_name.View(),
-			"\n\n(esc to quit)",
+	case "input_class_name":
+		s = ""
+		for i := 0; i < 2; i++ {
+			s += x[i]
+		}
+
+		s += fmt.Sprintf("\n%s%s",
+			"Press [Enter] to accept the following class name for your controller, else change it:\n\n",
+			m.custom_class_name.View(),
 		)
-	case 1:
-		s += "You chose your controller name as > \"" + m.textInput.Value() + "\"\n\n"
+		s += tail
 
-		s += "Great! We've generated a few options for your controller class name in the cpp files.\n"
-		s += "Choose one of the following name for the cpp class:\n\n"
-
-		for i, choice := range m.cpp_names {
-			cursor := " "
-			if m.cursor == i {
-				cursor = ">"
-			}
-			checked := " "
-
-			s += fmt.Sprintf("%s [%s] %s.cpp\n", cursor, checked, choice)
-		}
-		if m.cursor != len(m.cpp_names)-1 {
-			s += "\n\n You'd like your cpp name to be " + m.cpp_names[m.cursor] + ".cpp\n"
-		} else {
-			s += "\n\n Choose this option to enter custom cpp class name for controller\n"
-		}
-		s += "\n\n\n(use up and down arrows to select, enter to enter, esc to quit)\n"
-	default:
+	case "input_controller_name":
 		s += fmt.Sprintf(
-			"You're about to create a new controller for the FR3 Franka Research Robot\nWhat would you like to name your controller?\n%s\n\n%s",
-			m.textInput.View(),
-			"(esc to quit)",
-		) + "\n"
+			"You're about to create a new controller for the FR3 Franka Research Robot\nWhat would you like to name your controller?\n%s\n\n",
+			m.textInput.View())
+		s += tail
 	}
 	return s
 }
 
 func create_launch(path string, template_controller_name string, new_controller_name string) (err_ error) {
 	//copy file first
-	fmt.Println(path + template_controller_name + ".launch")
+	// fmt.Println(path + template_controller_name + ".launch")
 	source, err := os.Open(path + template_controller_name + ".launch")
 	if err != nil {
 		return err
 	}
 	defer source.Close()
-	fmt.Println("Created and Modified all Files\n")
+	// fmt.Println("Created and Modified all Files\n")
 
 	destination, err := os.Create(path + new_controller_name + ".launch")
 	if err != nil {
@@ -395,6 +432,8 @@ func create_source(path string, template_controller_name string, new_controller_
 
 		if strings.Contains(scanner.Text(), template_code_name) {
 			destination.WriteString(strings.Replace(scanner.Text(), template_code_name, new_code_name, -1) + "\n")
+		} else if strings.Contains(scanner.Text(), template_controller_name) {
+			destination.WriteString(strings.Replace(scanner.Text(), template_controller_name, new_controller_name, -1) + "\n")
 		} else if !(strings.Contains(scanner.Text(), "PLUGINLIB_EXPORT_CLASS")) {
 			destination.WriteString(scanner.Text() + "\n")
 		} else {
@@ -426,6 +465,8 @@ func create_header(path string, template_controller_name string, new_controller_
 
 		if strings.Contains(scanner.Text(), template_code_name) {
 			destination.WriteString(strings.Replace(scanner.Text(), template_code_name, new_code_name, -1) + "\n")
+		} else if strings.Contains(scanner.Text(), template_controller_name) {
+			destination.WriteString(strings.Replace(scanner.Text(), template_controller_name, new_controller_name, -1) + "\n")
 		} else if !(strings.Contains(scanner.Text(), "// namespace fr3_ros")) {
 			destination.WriteString(scanner.Text() + "\n")
 		} else {
@@ -437,15 +478,15 @@ func create_header(path string, template_controller_name string, new_controller_
 	return err
 }
 
-func add_controller_yaml(path string, template_controller_name string, new_controller_name string) (err_ error) {
+func add_controller_yaml(path string, backup_path string, template_controller_name string, new_controller_name string) (err_ error) {
 	//open main yaml file, create a temp file and a new destination file
 
-	e := os.Rename(path+"fr3_ros.yaml", path+"fr3_ros.yaml"+".bak")
+	e := os.Rename(path+"fr3_ros.yaml", path+"fr3_ros.yaml"+".backup")
 	if e != nil {
 		log.Fatal(e)
 	}
 
-	f, err1 := os.Open(path + "fr3_ros.yaml.bak")
+	f, err1 := os.Open(path + "fr3_ros.yaml.backup")
 	if err1 != nil {
 		return err1
 	}
@@ -495,7 +536,7 @@ func add_controller_yaml(path string, template_controller_name string, new_contr
 	f.Close()
 	f2.Close()
 
-	f_, err1 := os.Open(path + "fr3_ros.yaml.bak")
+	f_, err1 := os.Open(path + "fr3_ros.yaml.backup")
 	if err1 != nil {
 		return err1
 	}
@@ -532,17 +573,22 @@ func add_controller_yaml(path string, template_controller_name string, new_contr
 	if e != nil {
 		log.Fatal(e)
 	}
+
+	err := os.Rename(path+"fr3_ros.yaml.backup", backup_path+"fr3_ros.yaml.backup")
+	if err != nil {
+		log.Fatal(err)
+	}
 	return
 
 }
 
-func add_plugin(path string, new_controller_name string, controller_description string) (err_ error) {
-	e := os.Rename(path+"fr3_ros_plugin.xml", path+"fr3_ros_plugin.xml"+".bak")
+func add_plugin(path string, backup_path string, new_controller_name string, controller_description string) (err_ error) {
+	e := os.Rename(path+"fr3_ros_plugin.xml", path+"fr3_ros_plugin.xml"+".backup")
 	if e != nil {
 		log.Fatal(e)
 	}
 
-	f, err1 := os.Open(path + "fr3_ros_plugin.xml.bak")
+	f, err1 := os.Open(path + "fr3_ros_plugin.xml.backup")
 	if err1 != nil {
 		return err1
 	}
@@ -568,12 +614,17 @@ func add_plugin(path string, new_controller_name string, controller_description 
 
 	f.Close()
 	f2.Close()
+
+	err := os.Rename(path+"fr3_ros_plugin.xml.backup", backup_path+"fr3_ros_plugin.xml.backup")
+	if err != nil {
+		log.Fatal(err)
+	}
 	return
 
 }
-func add_lib_CMAKE(path string, controller_name string) (err_ error) {
+func add_lib_CMAKE(path string, backup_path string, controller_name string) (err_ error) {
 
-	e := os.Rename(path+"CMakeLists.txt", path+"CMakeLists.txt"+".bak")
+	e := os.Rename(path+"CMakeLists.txt", path+"CMakeLists.txt"+".backup")
 	if e != nil {
 		log.Fatal(e)
 	}
@@ -583,7 +634,7 @@ func add_lib_CMAKE(path string, controller_name string) (err_ error) {
 		return err2
 	}
 
-	source, err := os.Open(path + "CMakeLists.txt.bak")
+	source, err := os.Open(path + "CMakeLists.txt.backup")
 	if err != nil {
 		return err
 	}
@@ -600,6 +651,11 @@ func add_lib_CMAKE(path string, controller_name string) (err_ error) {
 
 	source.Close()
 	f2.Close()
+
+	err = os.Rename(path+"CMakeLists.txt.backup", backup_path+"CMakeLists.txt.backup")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	return
 
